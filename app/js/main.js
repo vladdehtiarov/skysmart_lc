fb7c1532d41e76714970831dbbdbaa5f67d45ad6
vladdehtiarov <vladdegtarov1998@gmail.com>
2021-08-01 23:56:19 +0300

    new page

--- app/js/main.js
+++ app/js/main.js
@@ -2,10 +2,13 @@
 const burgerImg = document.querySelector('.burger');
 const menuMob = document.querySelector('.header__menu-mob');
 
-burgerImg.addEventListener('click', function() {
-    menuMob.classList.toggle('show');
-    this.classList.toggle('close-burger');
-});
+if (burgerImg) {
+    burgerImg.addEventListener('click', function() {
+        menuMob.classList.toggle('show');
+        this.classList.toggle('close-burger');
+    });
+}
+
 
 
 // Filter for year on mobile
@@ -14,11 +17,13 @@ const cursesFiltersLeft = document.querySelector('.curses__filters-left');
 const backAgeFilter = document.querySelector('.back-age-filter');
 const showCourses = document.querySelector('.show-courses');
 
+if (anyYearsBtn) {
+    showElement(cursesFiltersLeft, anyYearsBtn, 'show');
 
-showElement(cursesFiltersLeft, anyYearsBtn, 'show');
+    hideElement(cursesFiltersLeft, backAgeFilter, 'show');
+    hideElement(cursesFiltersLeft, showCourses, 'show');
+}
 
-hideElement(cursesFiltersLeft, backAgeFilter, 'show');
-hideElement(cursesFiltersLeft, showCourses, 'show');
 
 function showElement(element, btn, selector) {
     btn.addEventListener('click', e => {
@@ -31,6 +36,19 @@ function hideElement(element, btn, selector) {
     btn.addEventListener('click', e => {
         e.preventDefault();
         element.classList.remove(selector);
+    });
+}
+
+//All age filters
+const allAgeBtn = document.querySelector('.all-age');
+const hiddenCheckbox = document.querySelectorAll('.ch-h');
+
+if (allAgeBtn) {
+    allAgeBtn.addEventListener('click', e => {
+        e.preventDefault();
+        hiddenCheckbox.forEach(i => {
+            i.classList.toggle('hide');
+        });
     });
 }
 
